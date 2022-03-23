a0c375865707109967643eb1870deaba4eff2c91
armaBurton <armanarma@armaburton.com>
2022-03-23 16:15:52 -0700

    branch for merging

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,22 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+	title,
+    COUNT(*)
+FROM
+	film
+INNER JOIN
+	inventory
+ON
+	film.film_id = inventory.film_id
+INNER JOIN
+	rental
+ON
+	inventory.inventory_id = rental.inventory_id
+GROUP BY 
+	film.film_id
+ORDER BY
+	COUNT DESC
+LIMIT
+	10;
--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,12 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT 
+	category.name,
+    COUNT(rental.rental_id),
+    RANK() OVER(
+      ORDER BY COUNT(rental.rental_id) DESC, SUM(rental.rental_id) DESC) FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film.film_id = film_category.film_id
+LEFT JOIN inventory ON inventory.film_id = film.film_id
+LEFT JOIN rental ON rental.inventory_id = inventory.inventory_id
+GROUP BY category.name
